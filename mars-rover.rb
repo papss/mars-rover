00edc97a109eda77908d086de99a2ee93f930cb1
Joseph Papaluca <jpapaluc@gmail.com>
2017-01-18 22:20:40 -0500

    old code version

--- mars-rover.rb
+++ mars-rover.rb
@@ -27,37 +27,13 @@ class Rover
     if instruction == "L"
       turn_left
     elsif instruction == "R"
-      turn_right
+      right
     else instruction == "M"
       forward
     end
   end
 
   #MOVEMENT METHODS
-
-  def turn_right
-    if @direction == "N"
-      @direction = "E"
-    elsif @direction == "E"
-      @direction = "S"
-    elsif @direction == "S"
-      @direction = "W"
-    else @direction == "W"
-      @direction = "N"
-    end
-  end
-
-  def turn_left
-    if @direction == "N"
-      @direction = "W"
-    elsif @direction == "W"
-      @direction = "S"
-    elsif @direction == "S"
-      @direction = "E"
-    else @direction == "E"
-      @direction = "N"
-    end
-  end
 
 
   def forward
@@ -71,13 +47,61 @@ class Rover
       @x = x - 1
     end
   end
-
-
 end
-
+end
 
 
 
 spirit = Rover.new("Spirit", 0, 0, "E")
 
 puts spirit.instructions("RMLMMM")
+
+
+#things that didn't work/weren't what we needed:
+
+#ORIGINAL TURNING METHODS
+
+# def right
+#   if @direction == "N"
+#     @direction = "E"
+#   elsif @direction == "E"
+#     @direction = "S"
+#   elsif @direction == "S"
+#     @direction = "W"
+#   else @direction == "W"
+#     @direction = "N"
+#   end
+# end
+#
+# def turn_left
+#   if @direction == "N"
+#     @direction = "W"
+#   elsif @direction == "W"
+#     @direction = "S"
+#   elsif @direction == "S"
+#     @direction = "E"
+#   else @direction == "E"
+#     @direction = "N"
+#   end
+# end
+
+#ORGINAL MOVEMENT METHOD-- this actually works fine but the assignment doesn't require manually entering
+#                          individual inputs
+
+# def forward
+#  puts "How would you like to move?"
+#  input = gets.chomp
+#   if input == "Move" && @direction == "N"
+#     @y = y + 1
+#     return "#{@name}'s position is #{@x}, #{@y}, facing #{@direction}.'"
+#   elsif input == "Move" && @direction == "S"
+#     @y = y - 1
+#     return "#{@name}'s position is #{@x}, #{@y}, facing #{@direction}.'"
+#   elsif input == "Move" && @direction == "E"
+#     @x = x + 1
+#     return "#{@name}'s position is #{@x}, #{@y}, facing #{@direction}.'"
+#   else input == "Move" && @direction == "W"
+#     @x = x - 1
+#     return "#{@name}'s position is #{@x}, #{@y}, facing #{@direction}.'"
+#   end
+# end
