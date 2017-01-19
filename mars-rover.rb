c966bdcae3193b0c9e6322dae32fc8c2bfaf1c77
Joseph Papaluca <jpapaluc@gmail.com>
2017-01-18 22:11:21 -0500

    turn right not working

--- mars-rover.rb
+++ mars-rover.rb
@@ -9,6 +9,32 @@ class Rover
     @direction = direction
   end
 
+  # def mars
+  #   puts "Mars is big. How big?"
+  #   mars = gets.chomp
+  #
+  #   mars = mars.chars
+  #   mars.map! { |number|
+  #     number.strip
+  #   }
+  #    mars.delete_if { |number|
+  #      number ==""
+  #   }
+
+  def instructions(input)
+    input.chars.each do |instruction|
+      puts instruction
+    if instruction == "L"
+      turn_left
+    elsif instruction == "R"
+      turn_right
+    else instruction == "M"
+      forward
+    end
+  end
+
+  #MOVEMENT METHODS
+
   def turn_right
     if @direction == "N"
       @direction = "E"
@@ -35,20 +61,14 @@ class Rover
 
 
   def forward
-    puts "turn Right, Left, or Move foward?"
-    input = gets.chomp
-    if input == "Move" && @direction == "N"
+    if @direction == "N"
       @y = y + 1
-      return "#{@name}'s position is #{@x}, #{@y}, facing #{@direction}.'"
-    elsif input == "Move" && @direction == "S"
+    elsif @direction == "S"
       @y = y - 1
-      return "#{@name}'s position is #{@x}, #{@y}, facing #{@direction}.'"
-    elsif input == "Move" && @direction == "E"
+    elsif @direction == "E"
       @x = x + 1
-      return "#{@name}'s position is #{@x}, #{@y}, facing #{@direction}.'"
-    else input == "Move" && @direction == "W"
+    else @direction == "W"
       @x = x - 1
-      return "#{@name}'s position is #{@x}, #{@y}, facing #{@direction}.'"
     end
   end
 
@@ -60,13 +80,4 @@ end
 
 spirit = Rover.new("Spirit", 0, 0, "E")
 
-puts spirit.turn_left
-
-
-  # def plateau
-  #   puts "how long is the x-axis?"
-  #     x_height = gets.chomp
-  #   puts "how long is the y-axis?"
-  #     y_length = get.chomp
-  #   plateau = x_height * y_length
-  # end
+puts spirit.instructions("RMLMMM")
